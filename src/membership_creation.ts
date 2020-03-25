b25b747bce61280ec7e2527d61b0aa45bc7ac152
Gleb Urvanov <gleb.urvanov@hotovo.org>
2020-03-25 10:43:43 +0100

    sign and send promise added

--- src/membership_creation.ts
+++ src/membership_creation.ts
@@ -1,9 +1,11 @@
 import { ApiPromise, WsProvider } from '@polkadot/api';
 import { registerJoystreamTypes } from '@joystream/types';
 import { Keyring } from '@polkadot/keyring';
-import { UserInfo, PaidTermId } from '@joystream/types/lib/members';
+import { UserInfo } from '@joystream/types/lib/members';
 import { assert } from 'chai';
 import BN from 'bn.js';
+import { SubmittableExtrinsic } from '@polkadot/api/types';
+import { KeyringPair } from '@polkadot/keyring/types';
 
 let api: ApiPromise;
 let nonce: BN;
@@ -24,44 +26,74 @@ describe('Membership integration tests', function() {
   it('Buy membeship is accepted with sufficient funds and rejected with insufficient', async function() {
     const alice = keyring.addFromUri('//Alice');
     const bob = keyring.addFromUri('//Bob');
-    await api.tx.members
-      .buyMembership(
+
+    await signAndSend(
+      api.tx.members.buyMembership(
         0,
         new UserInfo({ handle: 'alice_member', avatar_uri: '', about: '' })
-      )
-      .signAndSend(alice, { nonce });
+      ),
+      alice,
+      nonce
+    );
     nonce = nonce.add(new BN(1));
     let aliceMembership = await api.query.members.memberIdByAccountId(
       alice.address
     );
     assert(!aliceMembership.isEmpty, 'Alice is not a member');
     //TODO ensure bob's balance is insufficient to buy membership but sufficient to pay fee
-    await api.tx.members
-      .buyMembership(
+    await signAndSend(
+      api.tx.members.buyMembership(
         0,
         new UserInfo({ handle: 'bob_member', avatar_uri: '', about: '' })
-      )
-      .signAndSend(bob);
+      ),
+      bob,
+      nonce
+    );
     let bobMembership = await api.query.members.memberIdByAccountId(
       bob.address
     );
     assert(bobMembership.isEmpty, 'Bob is a member');
     //TODO membership cost and estimated fee should be retrived from chain
-    await api.tx.balances
-      .transfer(bob.address, 200)
-      .signAndSend(alice, { nonce });
+    await signAndSend(api.tx.balances.transfer(bob.address, 200), alice, nonce);
     //TODO ensure bob's balance is insufficient to buy membership
-    await api.tx.members
-      .buyMembership(
+    await signAndSend(
+      api.tx.members.buyMembership(
         0,
         new UserInfo({ handle: 'bob_member', avatar_uri: '', about: '' })
-      )
-      .signAndSend(bob, { nonce });
+      ),
+      bob,
+      nonce
+    );
     bobMembership = await api.query.members.memberIdByAccountId(bob.address);
     assert(!bobMembership.isEmpty, 'Bob is a not member');
-  });
+  }).timeout(30000);
 
   after(function() {
     api.disconnect();
   });
 });
+
+async function signAndSend(
+  tx: SubmittableExtrinsic<'promise'>,
+  account: KeyringPair,
+  nonce: BN
+) {
+  await new Promise(async (resolve, reject) => {
+    const signedTx = tx.sign(account, { nonce });
+
+    await tx
+      .send(async result => {
+        if (result.status.isFinalized == true) {
+          console.log('tx succeed');
+          resolve(true);
+        } else if (result.status.isInvalid) {
+          console.log('tx is invalid');
+          resolve(true);
+        }
+      })
+      .catch(error => {
+        console.log('error during tx sending');
+        reject(error);
+      });
+  });
+}
