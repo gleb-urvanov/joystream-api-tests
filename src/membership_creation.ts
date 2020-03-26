2d44e37755724cbd921cadea669bf4fce3846184
Gleb Urvanov <gleb.urvanov@hotovo.org>
2020-03-26 14:55:54 +0100

    code cleaning, multiaccount implementation started

--- src/membership_creation.ts
+++ src/membership_creation.ts
@@ -1,86 +1,99 @@
 import { ApiPromise, WsProvider } from '@polkadot/api';
 import { registerJoystreamTypes } from '@joystream/types';
 import { Keyring } from '@polkadot/keyring';
-import {
-  UserInfo,
-  PaidMembershipTerms,
-  PaidTermId
-} from '@joystream/types/lib/members';
+import { UserInfo, PaidMembershipTerms } from '@joystream/types/lib/members';
 import { assert } from 'chai';
 import { KeyringPair } from '@polkadot/keyring/types';
-import { signAndSend, getNonce } from './utils';
+import { Utils } from './utils';
 import { Option } from '@polkadot/types';
-
-let api: ApiPromise;
-let keyring: Keyring;
+import { Balance } from '@polkadot/types/interfaces';
 
 describe('Membership integration tests', function() {
+  let api: ApiPromise;
+  const keyring = new Keyring({ type: 'sr25519' });
+  let alice: KeyringPair;
+  let keyPairs: Array<KeyringPair>;
+
   before(async function() {
     registerJoystreamTypes();
     const provider = new WsProvider('ws://127.0.0.1:9944');
     api = await ApiPromise.create({ provider });
-    keyring = new Keyring({ type: 'sr25519' });
+    alice = keyring.addFromUri('//Alice');
+    keyPairs = new Array();
+    keyPairs.push(keyring.addFromUri('1'));
   });
 
   it('Buy membeship is accepted with sufficient funds and rejected with insufficient', async function() {
-    let alice = keyring.addFromUri('//Alice');
-    let charlie = keyring.addFromUri('//Charlie');
-    let paidTerms = await api.query.members.paidMembershipTermsById<
-      Option<PaidMembershipTerms>
-    >(0);
-    console.log('terms fee ' + paidTerms.unwrap().fee);
-    await transferBalance(api, alice, charlie.address, 2);
+    let membershipFee = (
+      await api.query.members.paidMembershipTermsById<
+        Option<PaidMembershipTerms>
+      >(0)
+    )
+      .unwrap()
+      .fee.toNumber();
+    await transferBalance(api, alice, keyPairs[0].address, 2);
     await buyMembership(api, alice, 0, 'alice_member');
     let aliceMembership = await getMembership(alice.address);
     assert(!aliceMembership.isEmpty, 'Alice is not a member');
-    //TODO ensure bob's balance is insufficient to buy membership
-    await buyMembership(api, charlie, 0, 'charlie_member', true);
-    let bobMembership = await getMembership(charlie.address);
-    assert(bobMembership.isEmpty, 'Charlie is a member');
-    //TODO membership cost should be retrived from chain
-    await transferBalance(api, alice, charlie.address, 100);
-    //TODO ensure bob's balance is sufficient to buy membership
-    await buyMembership(api, charlie, 0, 'charlie_member');
-    bobMembership = await getMembership(charlie.address);
-    assert(!bobMembership.isEmpty, 'Charlie is a not member');
+    let accountBalance = (await getBalance(keyPairs[0].address)).toNumber();
+    assert(
+      accountBalance < membershipFee,
+      'The account already have sufficient balance to purchase membership'
+    );
+    await buyMembership(api, keyPairs[0], 0, 'charlie_member', true);
+    let charlieMembership = await getMembership(keyPairs[0].address);
+    assert(charlieMembership.isEmpty, 'Charlie is a member');
+    await transferBalance(api, alice, keyPairs[0].address, membershipFee);
+    accountBalance = (await getBalance(keyPairs[0].address)).toNumber();
+    assert(
+      accountBalance >= membershipFee,
+      'The account balance is insufficient to purchase membership'
+    );
+    await buyMembership(api, keyPairs[0], 0, 'charlie_member');
+    charlieMembership = await getMembership(keyPairs[0].address);
+    assert(!charlieMembership.isEmpty, 'Charlie is a not member');
   }).timeout(60000);
 
   after(function() {
     api.disconnect();
   });
+
+  async function buyMembership(
+    api: ApiPromise,
+    account: KeyringPair,
+    paidTerms: number,
+    name: string,
+    expectFailure = false
+  ) {
+    return Utils.signAndSend(
+      api.tx.members.buyMembership(
+        paidTerms,
+        new UserInfo({ handle: name, avatar_uri: '', about: '' })
+      ),
+      account,
+      await Utils.getNonce(account, api),
+      expectFailure
+    );
+  }
+
+  async function getMembership(address: string) {
+    return api.query.members.memberIdsByControllerAccountId(address);
+  }
+
+  async function getBalance(address: string): Promise<Balance> {
+    return api.query.balances.freeBalance<Balance>(address);
+  }
+
+  async function transferBalance(
+    api: ApiPromise,
+    from: KeyringPair,
+    to: string,
+    amount: number
+  ) {
+    return Utils.signAndSend(
+      api.tx.balances.transfer(to, amount),
+      from,
+      await Utils.getNonce(from, api)
+    );
+  }
 });
-
-async function buyMembership(
-  api: ApiPromise,
-  account: KeyringPair,
-  paidTerms: number,
-  name: string,
-  expectFailure = false
-) {
-  return signAndSend(
-    api.tx.members.buyMembership(
-      paidTerms,
-      new UserInfo({ handle: name, avatar_uri: '', about: '' })
-    ),
-    account,
-    await getNonce(account, api),
-    expectFailure
-  );
-}
-
-async function getMembership(address: string) {
-  return api.query.members.memberIdsByControllerAccountId(address);
-}
-
-async function transferBalance(
-  api: ApiPromise,
-  from: KeyringPair,
-  to: string,
-  amount: number
-) {
-  return signAndSend(
-    api.tx.balances.transfer(to, amount),
-    from,
-    await getNonce(from, api)
-  );
-}
--- src/utils.ts
+++ src/utils.ts
@@ -3,40 +3,42 @@ import { SubmittableExtrinsic } from '@polkadot/api/types';
 import { ApiPromise } from '@polkadot/api';
 import BN = require('bn.js');
 
-export async function signAndSend(
-  tx: SubmittableExtrinsic<'promise'>,
-  account: KeyringPair,
-  nonce: BN,
-  expectFailure = false
-) {
-  await new Promise(async (resolve, reject) => {
-    const signedTx = tx.sign(account, { nonce });
+export class Utils {
+  public static async signAndSend(
+    tx: SubmittableExtrinsic<'promise'>,
+    account: KeyringPair,
+    nonce: BN,
+    expectFailure = false
+  ) {
+    return new Promise(async (resolve, reject) => {
+      const signedTx = tx.sign(account, { nonce });
 
-    console.log('tx signed for ' + account.address);
-    await signedTx
-      .send(async result => {
-        if (result.status.isFinalized == true && result.events != undefined) {
-          result.events.forEach(event => {
-            if (event.event.method == 'ExtrinsicFailed') {
-              if (expectFailure) {
-                resolve();
-              } else {
-                reject(new Error('Extrinsic failed unexpectedly'));
+      console.log('tx signed for ' + account.address);
+      await signedTx
+        .send(async result => {
+          if (result.status.isFinalized == true && result.events != undefined) {
+            result.events.forEach(event => {
+              if (event.event.method == 'ExtrinsicFailed') {
+                if (expectFailure) {
+                  resolve();
+                } else {
+                  reject(new Error('Extrinsic failed unexpectedly'));
+                }
               }
-            }
-          });
-          resolve();
-        }
-      })
-      .catch(error => {
-        reject(error);
-      });
-  });
-}
+            });
+            resolve();
+          }
+        })
+        .catch(error => {
+          reject(error);
+        });
+    });
+  }
 
-export async function getNonce(account: KeyringPair, api: ApiPromise) {
-  let nonceString = (
-    await api.query.system.accountNonce(account.address)
-  ).toString();
-  return new BN(nonceString);
+  public static async getNonce(account: KeyringPair, api: ApiPromise) {
+    let nonceString = (
+      await api.query.system.accountNonce(account.address)
+    ).toString();
+    return new BN(nonceString);
+  }
 }
