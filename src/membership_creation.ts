db89faa86322170a02f7e9f270f40efdaaf651e1
Gleb Urvanov <gleb.urvanov@hotovo.org>
2020-03-26 12:58:16 +0100

    made compatible with the actual runtime

--- src/membership_creation.ts
+++ src/membership_creation.ts
@@ -1,10 +1,15 @@
 import { ApiPromise, WsProvider } from '@polkadot/api';
 import { registerJoystreamTypes } from '@joystream/types';
 import { Keyring } from '@polkadot/keyring';
-import { UserInfo } from '@joystream/types/lib/members';
+import {
+  UserInfo,
+  PaidMembershipTerms,
+  PaidTermId
+} from '@joystream/types/lib/members';
 import { assert } from 'chai';
 import { KeyringPair } from '@polkadot/keyring/types';
 import { signAndSend, getNonce } from './utils';
+import { Option } from '@polkadot/types';
 
 let api: ApiPromise;
 let keyring: Keyring;
@@ -19,21 +24,25 @@ describe('Membership integration tests', function() {
 
   it('Buy membeship is accepted with sufficient funds and rejected with insufficient', async function() {
     let alice = keyring.addFromUri('//Alice');
-    let bob = keyring.addFromUri('//Bob');
-    await transferBalance(api, alice, bob.address, 2);
+    let charlie = keyring.addFromUri('//Charlie');
+    let paidTerms = await api.query.members.paidMembershipTermsById<
+      Option<PaidMembershipTerms>
+    >(0);
+    console.log('terms fee ' + paidTerms.unwrap().fee);
+    await transferBalance(api, alice, charlie.address, 2);
     await buyMembership(api, alice, 0, 'alice_member');
     let aliceMembership = await getMembership(alice.address);
     assert(!aliceMembership.isEmpty, 'Alice is not a member');
     //TODO ensure bob's balance is insufficient to buy membership
-    await buyMembership(api, bob, 0, 'bob_member', true);
-    let bobMembership = await getMembership(bob.address);
-    assert(bobMembership.isEmpty, 'Bob is a member');
+    await buyMembership(api, charlie, 0, 'charlie_member', true);
+    let bobMembership = await getMembership(charlie.address);
+    assert(bobMembership.isEmpty, 'Charlie is a member');
     //TODO membership cost should be retrived from chain
-    await transferBalance(api, alice, bob.address, 100);
+    await transferBalance(api, alice, charlie.address, 100);
     //TODO ensure bob's balance is sufficient to buy membership
-    await buyMembership(api, bob, 0, 'bob_member');
-    bobMembership = await getMembership(bob.address);
-    assert(!bobMembership.isEmpty, 'Bob is a not member');
+    await buyMembership(api, charlie, 0, 'charlie_member');
+    bobMembership = await getMembership(charlie.address);
+    assert(!bobMembership.isEmpty, 'Charlie is a not member');
   }).timeout(60000);
 
   after(function() {
@@ -60,7 +69,7 @@ async function buyMembership(
 }
 
 async function getMembership(address: string) {
-  return api.query.members.memberIdByAccountId(address);
+  return api.query.members.memberIdsByControllerAccountId(address);
 }
 
 async function transferBalance(
